361b35a65c69a5c353f58d631a84704fa05bfb1c
iamyournikhil8 <t.nikhil@s.amity.edu>
2025-05-06 22:13:30 +0530

    sixth may 7th

--- 1.c
+++ 1.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    float weight, height, bmi;
+
+    // Input weight in kilograms
+    printf("Enter your weight in kilograms: ");
+    scanf("%f", &weight);
+
+    // Input height in meters
+    printf("Enter your height in meters: ");
+    scanf("%f", &height);
+
+    // Check for valid height
+    if (height <= 0) {
+        printf("Height must be greater than zero.\n");
+        return 1;
+    }
+
