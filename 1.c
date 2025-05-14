31da5156375a163c2acd78ddaef5264d3f709b89
iamyournikhil8 <t.nikhil@s.amity.edu>
2025-05-14 14:27:05 +0530

    struct to store bmi record

--- 1.c
+++ 1.c
@@ -4,4 +4,14 @@
 
 #define MAX_USERS 100
 
+// Struct to store BMI record
+typedef struct {
+    char name[50];
+    float height;
+    float weight;
+    float bmi;
+    char category[20];
+    char unit[10];
+} BMI_Record;
+
 
